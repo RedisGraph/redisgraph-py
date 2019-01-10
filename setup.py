eaa9cdf7b75121e4802c73dc5040b5ff672822f8
swilly22 <roi@redislabs.com>
2019-01-10 08:54:12 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='redisgraph',
-    version='1.5',
+    version='1.6',
 
     description='RedisGraph Python Client',
     url='https://github.com/redislabs/redisgraph-py',
