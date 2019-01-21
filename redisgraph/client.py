8e6a2ccf9c90bdc6db8f62f72aed7021ab3e5339
Shivanjan Chakravorty <schakravorty846@gmail.com>
2019-01-21 12:02:16 +0530

    Issue #15: MERGE support added (#17)
    * Issue #15: MERGE support added
    
    * Removed vague check
    
    Removed check
    `if object:`

--- redisgraph/client.py
+++ redisgraph/client.py
@@ -170,3 +170,13 @@ class Graph(object):
         Deletes graph.
         """
         return self.redis_con.execute_command("GRAPH.DELETE", self.name)
+    
+    def merge(self, pattern):
+        """
+        Merge a node or relation.
+        """
+
+        query = 'MERGE '
+        query += str(pattern)
+
+        return self.query(query)
