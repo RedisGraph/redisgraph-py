bf7e43dfd23105b88660af8a9484d5791d8eef04
Guy Korland <gkorland@gmail.com>
2019-12-16 20:25:20 +0200

    add long_description to package (#61)
    * add long_description to package
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup, find_packages
+import io
+
+def read_all(f):
+    with io.open(f, encoding="utf-8") as I:
+        return I.read()
+
+requirements = map(str.strip, open("requirements.txt").readlines())    
+    
 setup(
     name='redisgraph',
     version='2.1',
-
     description='RedisGraph Python Client',
+    long_description=read_all("README.md"),
+    long_description_content_type='text/markdown',
     url='https://github.com/redislabs/redisgraph-py',
     packages=find_packages(),
-    install_requires=['redis', 'PTable'],
+    install_requires=requirements,
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Database'
-    ]
+    ],
+    keywords='Redis Graph Extension',
+    author='RedisLabs',
+    author_email='oss@redislabs.com'
 )
