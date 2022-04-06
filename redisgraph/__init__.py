d88979008ca5e595ed36396e2e02f90bd26e7df6
dvora-h <67596500+dvora-h@users.noreply.github.com>
2022-04-06 10:40:27 +0300

    lint

--- redisgraph/__init__.py
+++ redisgraph/__init__.py
@@ -4,5 +4,9 @@ from .edge import Edge     # noqa
 from .graph import Graph   # noqa
 from .path import Path     # noqa
 
-warn("Please upgrade to redis-py (https://pypi.org/project/redis/) "
-"This library is deprecated, and all features have been merged into redis-py.", DeprecationWarning, stacklevel=2)
+warn(
+    "Please upgrade to redis-py (https://pypi.org/project/redis/) ",
+    "This library is deprecated, and all features have been merged into redis-py.",
+    DeprecationWarning,
+    stacklevel=2
+)
